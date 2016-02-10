c2e76bb0ee25794e703728d1b771683b8246d06b
Jesse Paterson <jesse.r.pat@gmail.com>
2016-02-09 20:36:09 -0800

    added app.use(express.static('public')); to the server.js -- Should allow it to serve up static content

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 
+app.use(express.static('assets'));
+
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
