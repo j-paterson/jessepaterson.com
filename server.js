f72551b68be68cee2ffb102bdabbbbd8e126e548
Jesse Paterson <jesse.r.pat@gmail.com>
2016-02-23 16:11:33 -0800

    updated server.js to include kickgrass.html

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ router.get("/projects",function(req,res){
   res.sendFile(path + "project_1.html");
 });
 
+router.get("/projects/kickgrass",function(req,res){
+  res.sendFile(path + "kickgrass.html");
+});
+
 router.get("/contact",function(req,res){
   res.sendFile(path + "root.html");
 });
