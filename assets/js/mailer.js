ab7bd3f59d275b3feebfcb23668d613668491010
Jesse Paterson <jesse.r.pat@gmail.com>
2016-05-06 01:19:36 -0700

    added contact section

---
+++ assets/js/mailer.js
@@ -0,0 +1,51 @@
+var nodemailer = require('nodemailer');
+var xoauth2 = require('xoauth2');
+
+// listen for token updates (if refreshToken is set)
+// you probably want to store these to a db
+generator.on('token', function(token){
+    console.log('New token for %s: %s', token.user, token.accessToken);
+});
+
+// login
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        xoauth2: xoauth2.createXOAuth2Generator({
+            user: '{username}',
+            clientId: '{Client ID}',
+            clientSecret: '{Client Secret}',
+            refreshToken: '{refresh-token}',
+            accessToken: '{cached access token}'
+        })
+    }
+});
+
+
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport('smtps://jpaterson.public@gmail.com:hoaxX2luv');
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '"Fred Foo 👥" <foo@blurdybloop.com>', // sender address
+    to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world 🐴', // plaintext body
+    html: '<b>Hello world 🐴</b>' // html body
+};
+
+if(document.getElementById('send-button').clicked == true)
+{
+   alert("button was clicked");
+}
+
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
