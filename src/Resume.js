577a91b1c2808f97c265b0463bc27c0bec883d83
Jesse Paterson <jesse.r.pat@gmail.com>
2017-09-18 20:30:34 -0700

    Resume appears to be working in dev...

--- src/Resume.js
+++ src/Resume.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
 import AnimatedWrapper from "./AnimatedWrapper";
 class ResumeComponent extends Component {
   render() {
     return (
       <div className="resume page">
         <div className="container">
-          <embed type='application/pdf' src="/jp_resume.pdf" width= "80%" height= "800px"></embed>
+          <embed type='application/pdf' src="/jp_resume.pdf" width= "100%" height= "800px"></embed>
         </div>
       </div>
     )
