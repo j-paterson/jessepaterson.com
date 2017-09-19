8371a4994c85a1f17306378825862c09485c2e40
Jesse Paterson <jesse.r.pat@gmail.com>
2017-09-18 17:55:59 -0700

    Merged about and experience

--- src/Header.js
+++ src/Header.js
@@ -33,7 +33,7 @@ export default class Header extends Component {
 	        <li><NavLink exact={true} to="/" activeClassName="active">Home</NavLink></li>
 	        <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
 	        <li><NavLink to="/projects" activeClassName="active">Projects</NavLink></li>
-	        <li><NavLink to="/experience" activeClassName="active">Experience</NavLink></li>
+	        {/* <li><NavLink to="/experience" activeClassName="active">Experience</NavLink></li> */}
           <li><a href="/jp_resume.pdf">Resume</a></li>
 	      </ul>
         <button className="c-hamburger c-hamburger--htx">
